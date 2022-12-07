98140e0faed70dea17e6d19664f41c078f56c96f
gymbundye <gymbundye@gmail.com>
2022-12-06 22:30:38 -0500

    made main container

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-import Main from './Components/Main'
+import Tenzie from './Components/Tenzie'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Main/>
+      <Tenzie/>
        
     </div>
   )
---
+++ src/Components/Tenzie.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Tenzie() {
+  return (
+    <main className='main'>
+      <div></div>
+    </main>
+  )
+}
+
+export default Tenzie

27e3d53d16ee5373285af4c9e34ef4e5cdfd0990
gymbundye <gymbundye@gmail.com>
2022-12-21 20:36:54 -0500

    added random to state

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,36 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
-import Tenzie from './Components/Tenzie'
+
 import Instructions from './Components/Instructions'
 import Die from './Components/Die'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      
-      <Instructions/>
-
-      <main>
-        
-        <div className='dice-box'>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        <Die value="1"/>
-        </div>
+function App() 
+  
+  {
+    const [dice, setDice] = React.useState(allNewDice())
     
-      </main>
-      
-       
-    </div>
-  )
-}
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(Math.ceil(Math.random() * 6))
+        }
+        return newDice
+    }
+    
+    const diceElements = dice.map(die => <Die value={die} />)
+    
+    return (
+        <div className='game'>
+
+          <Instructions/>
+        <main>
+            <div className="dice-box">
+                {diceElements}
+            </div>
+            </main>
+        </div>
+    )
+
+    }  
 
 export default App

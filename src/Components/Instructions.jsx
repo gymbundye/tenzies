73c4d02b823db96ba29b7b76dee980d4891c31fa
gymbundye <gymbundye@gmail.com>
2023-01-03 22:35:34 -0500

    added hold num a bility

--- src/Components/Instructions.jsx
+++ src/Components/Instructions.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 function Instructions() {
   return (
-    <div className='instructions'>
+    <div className='instructions-container'>
+    
     <h3><center>Tenzies</center></h3>
+
+    <p className='instructions'>Roll until all dice are the same.
+     Click each die to freeze it at its current value between rolls.
+     Have Fun!</p>
 
     </div>
   )
